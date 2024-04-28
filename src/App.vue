dfec1850a0f7edf43ca4d62aec5ba29a790a7206
Macailh <salvadorenriquegb@gmail.com>
2024-04-28 07:19:03 -0600

    Option API

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
-  <h1>Listado de posts:</h1>
-  <PostContainer />
+  <!-- <h1>Listado de posts:</h1>
+  <PostContainer /> -->
+  <CounterOption />
 </template>
 
 <script setup>
-import PostContainer from './components/PostContainer.vue'
+// import PostContainer from './components/PostContainer.vue'
+import CounterOption from './components/CounterOption.vue'
 </script>
 
 <style>
